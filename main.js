e646391c4d535786189f72b55f420a5611ffefc6
carlaweglin <weglin.carla@gmail.com>
2023-01-11 20:19:28 -0300

    agrega-funcion-boton-ocultar-filtros

--- main.js
+++ main.js
@@ -11,11 +11,14 @@ let vista_reportes = $('#vista-reportes');
 let btn_vista_balance = $('#btn-vista-balance');
 let btn_vista_categorias = $('#btn-vista-categorias');
 let btn_vista_reportes = $('#btn-vista-reportes');
-
+let btn_oculta_filtros = $('#oculta-filtros');
+let conteiner_filtros = $('#conteiner-filtros');
 
 
 
 //eventos
+
+//------------ botones vistas---------------//
 
 btn_vista_categorias.addEventListener("click", () => {
     vista_balance.classList.add("is-hidden");
@@ -34,3 +37,8 @@ btn_vista_balance.addEventListener("click", () => {
     vista_categorias.classList.add("is-hidden");
     vista_balance.classList.remove("is-hidden");
 });
+
+btn_oculta_filtros.addEventListener("click", () => {
+    conteiner_filtros.classList.toggle("is-hidden");
+});
+
